546072c8d632647c63ad6a03dfad9912732a756e
aryadamara <aryadamara@DESKTOP-PT9UTUQ>
2024-03-07 17:12:24 +0700

    membuat variabel

--- algoritma 1/algoritma 1.cpp
+++ algoritma 1/algoritma 1.cpp
@@ -45,3 +45,30 @@ void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bublle
         }
         pass = pass + 1; //step 4
 
+    } while (pass <= n); //step 5
+}
+
+void display() {
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; // Output setiap element array pada garis baru
+
+    }
+    cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+    cout << endl;
+}
+
+int main()
+{
+    input();  //memanggil prosedur untuk menginput data
+    bubbleSortArray();  //memanggil prosedur bubblesort untuk proses mengurutkan data
+    display();//memanggil prosedur untuk menampilkan data yang telah urut
+    system("pause");
+
+
+    return 0;
+}
+
